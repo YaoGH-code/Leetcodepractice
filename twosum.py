bca92398a2957404fe091b37a783bd84da168486
YaoGH-code <johnx9566@users.noreply.github.com>
2021-10-09 00:26:33 -0400

    two sum oct9

---
+++ twosum.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        for number in nums:
+            index = nums.index(number)
+            if target-number in nums[nums.index(number)+1:]:
+                if index == nums.index(target-number): #two
+                    return [nums.index(number), nums[index+1:].index(target-number)+1+index]
+                else:
+                    return [nums.index(number), nums.index(target-number)]
